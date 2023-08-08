5d5b8da8f0595178736c9830f4cf15a326a58e5b
JonathanAlexMasc <mascarenhasjonathan@gmail.com>
2023-08-08 17:07:44 -0600

    fixed icon issue

--- src/components/PreviewBlog.jsx
+++ src/components/PreviewBlog.jsx
@@ -47,9 +47,10 @@ export default function PreviewBlog() {
 
     return (
         <Container className="m-3">
-            <Container className="d-inline-flex mb-3">
-                <h1>Featured Posts</h1>
-                <i className="fa-solid fa-star fa-xl center mt-4 mx-3" style={{ color: "#ffd877", }}></i>
+            <Container className="d-inline-flex mb-3 align-items-evenly">
+                <div>
+                    <h1>Featured Posts <i className="fa-solid fa-star center mx-1" style={{ color: "#ffd877", }}></i></h1>
+                </div>
             </Container>
             <Row xs={1} md={2} className="g-4">
                 {blogsArray.map((singleBlog, idx) => (
