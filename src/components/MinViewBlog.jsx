7cd9526ca694275aa7eb0807430664218f1c3676
JonathanAlexMasc <mascarenhasjonathan@gmail.com>
2023-08-08 08:40:22 -0600

    yeah

--- src/components/MinViewBlog.jsx
+++ src/components/MinViewBlog.jsx
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Container, Image } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import axios from 'axios';
+async function getImage(id) {
+    const response = await axios.get(`http://localhost:1337/api/blogs/${id}?populate=*`);
+    const { data } = response;
+    const cover = data.data.attributes.cover;
+    const coverURL = cover.data.attributes.url;
+    //const coverImg = cover.data.attributes.formats.large;
+    // console.log('http://localhost:1337' + blogImg.url); - this is what I needed to load the //imageverImg
+    console.log(cover);
+    return coverURL;
+}
 
-export default function MinViewBlog({ id, title, description, author, date }) {
+export default function MinViewBlog({ id, title, author, date, preview }) {
+    const [blogImg, setBlogImg] = useState("");
+
+    useEffect(() => {
+        getImage(id).then(res => { setBlogImg(res) });
+    }, [id])
+
     return (
         <div>
             <Container className="d-flex flex-column flex-lg-row my-3 p-4 gap-5 ">
                 <Container fluid>
-                    <Image src={sunsetImg} fluid className="mr-4 img-responsive" style={{ width: "900px", height: "380px" }} />
+                    <Image src={'http://localhost:1337' + blogImg} fluid className="mr-4 img-responsive" style={{ width: "900px", height: "380px" }} />
                 </Container>
                 <Container fluid className="d-flex flex-column mr-5 gap-2 justify-content-evenly">
                     <div className="d-flex flex-row">
@@ -18,7 +35,7 @@ export default function MinViewBlog({ id, title, description, author, date }) {
                         <h2>{title}</h2>
                     </Link>
                     <h6 className="">
-                        {description}
+                        {preview}
                     </h6>
                     <h6 className=""><b>by {author}</b></h6>
                 </Container>
