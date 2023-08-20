196b0b70fdc5ef812959aa4216e56ce16c78866d
JonathanAlexMasc <mascarenhasjonathan@gmail.com>
2023-08-20 17:03:14 -0600

    design fixes

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -5,14 +5,14 @@ import moi from './moi.jpg';
 export default function ContactPage() {
 
     return (
-        <Container fluid className="d-flex flex-lg-row flex-column m-3 p-3 align-items-center">
+        <Container fluid className="d-flex flex-lg-row flex-column p-3 align-items-center">
             <Container fluid className="p-3">
-                <img src={moi} alt="pic of me" style={{ width: "600px", height: "800px" }}></img>
+                <img src={moi} alt="pic of me" className="img-fluid"></img>
             </Container>
             <Container fluid className="p-3 m-3 d-flex flex-column">
                 <h3 className="m-3">Please enter your information in this form to reach out to me.</h3>
 
-                <form name="contact" method="post" className="w-full m-3">
+                <form name="contact" method="post" className="m-3">
                     <input type="hidden" name="form-name" value="contact" />
                     <div hidden>
                         <input name="bot-field" />
