26357a8fe25cb51412f9b8d7ff438a7eb0eba36f
JonathanAlexMasc <mascarenhasjonathan@gmail.com>
2023-08-20 16:52:00 -0600

    sngosng

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,23 +9,30 @@ export default function Footer() {
                         <section className="mb-2">
 
                             <h6>
-                                <i className="fa-brands fa-facebook fa-2xl mx-2" style={{ color: "#ffffff" }}></i>
-
-                                <i className="fa-brands fa-twitter fa-2xl mx-2" style={{ color: "#ffffff" }}></i>
-
-                                <i className="fa-brands fa-google fa-2xl mx-2" style={{ color: "#ffffff" }}></i>
-
-                                <i className="fa-brands fa-instagram fa-2xl mx-2" style={{ color: "#ffffff" }}></i>
-
-                                <i className="fa-brands fa-linkedin fa-2xl mx-2" style={{ color: "#ffffff" }}></i>
-
-                                <i className="fa-brands fa-github fa-2xl mx-2" style={{ color: "#ffffff" }}></i>
+                                <a href='https://www.facebook.com/jonathan.mascarenhas.37/'>
+                                    <i className="fa-brands fa-facebook fa-2xl mx-2" style={{ color: "#ffffff" }}></i>
+                                </a>
+                                <a href='https://twitter.com/BusinessJon'>
+                                    <i className="fa-brands fa-twitter fa-2xl mx-2" style={{ color: "#ffffff" }}></i>
+                                </a>
+                                {/* <a>
+                                    <i className="fa-brands fa-google fa-2xl mx-2" style={{ color: "#ffffff" }}></i>
+                                </a> */}
+                                <a href='https://www.instagram.com/jonathan_mascarenhas/'>
+                                    <i className="fa-brands fa-instagram fa-2xl mx-2" style={{ color: "#ffffff" }}></i>
+                                </a>
+                                <a href='https://www.linkedin.com/in/jonathanmascarenhas/'>
+                                    <i className="fa-brands fa-linkedin fa-2xl mx-2" style={{ color: "#ffffff" }}></i>
+                                </a>
+                                <a href='https://github.com/JonathanAlexMasc'>
+                                    <i className="fa-brands fa-github fa-2xl mx-2" style={{ color: "#ffffff" }}></i>
+                                </a>
                             </h6>
                         </section>
                     </div>
 
                     <div className="text-center text-lg text-white p-3">
-                        © 2020 Copyright:
+                        © 2023 Copyright:
                         <a className="text-lg text-white m-1" href="/">The Goan Traveller</a>
                     </div>
                 </footer>
